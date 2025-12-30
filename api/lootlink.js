671cd5faba6742ae485783cf658536d9acf366f7
tobyxxzz <pedrolulapedra@gmail.com>
2025-12-30 19:06:08 -0300

    Update lootlink.js

--- api/lootlink.js
+++ api/lootlink.js
@@ -1,11 +1,5 @@
 export default function handler(req, res) {
-  const { data } = req.query;
-
-  if (!data) {
-    return res.redirect("https://lootdest.org/s?rokoJQbL");
-  }
-
   return res.redirect(
-    `https://systemkey.vercel.app/api/getkey?data=${data}`
+    "https://systemkey.vercel.app/api/getkey"
   );
 }
