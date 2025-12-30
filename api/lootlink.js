aa7173abf8ba6067026745f0aeaf4f680f28f8ab
tobyxxzz <pedrolulapedra@gmail.com>
2025-12-30 18:54:41 -0300

    Update lootlink.js

--- api/lootlink.js
+++ api/lootlink.js
@@ -1,12 +1,11 @@
 export default function handler(req, res) {
   const { data } = req.query;
 
-  // se não veio do lootlabs, volta pro locker
   if (!data) {
-    return res.redirect("https://lootlabs.gg/s/rokoJQbL");
+    return res.redirect("https://lootdest.org/s?rokoJQbL");
   }
 
-  // veio certinho → manda pro getkey
-return res.redirect
-  (`https://TEU-PROJETO.vercel.app/api/getkey?data=${data}`);
+  return res.redirect(
+    `https://systemkey.vercel.app/api/getkey?data=${data}`
+  );
 }
