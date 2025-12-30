0ab5cddca463220d8f7a0d424736276698908c01
tobyxxzz <pedrolulapedra@gmail.com>
2025-12-30 19:13:04 -0300

    Update lootlink.js

--- api/lootlink.js
+++ api/lootlink.js
@@ -1,4 +1,9 @@
 export default function handler(req, res) {
+  // cookie válido por 5 minutos (tempo suficiente pra pegar a key)
+  res.setHeader("Set-Cookie",
+    "passed=true; Max-Age=300; Path=/; HttpOnly; SameSite=Lax"
+  );
+
   return res.redirect(
     "https://systemkey.vercel.app/api/getkey"
   );
